81f3a8a0643252acfe35f3754e451bdb3f82a617
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-03-22 15:51:22 +0530

    2nd commit

--- app.js
+++ app.js
@@ -13,7 +13,40 @@ app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
   res.render('homepage.ejs');
+  // exec(`adb devices`)
 })
+
+
+
+app.get('/connecteddevices',(req,res)=>{
+    res.render('connecteddevices.ejs');
+})
+
+
+//connected devices 
+app.post('/connecteddevices',(req,res)=>{
+    // res.send('post request successful!') 
+    exec(`adb devices`,(error,stdout,stderr)=>{
+      //error handling
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).send('Error occurred');
+      }
+      if (stderr) {
+        console.error(`ADB Error: ${stderr}`);
+        return res.status(500).send('ADB Error occurred');
+      }
+    
+      //showing the devices
+      const devices = stdout.split('\n').slice(1).filter(line => line.trim() !== '').map(line => {
+        const [device, state] = line.trim().split('\t');
+        return { device, state };
+      });
+
+      res.send(devices);
+    }) 
+});
+
 
 app.get('/pairdevice',(req,res)=>{
   res.render('pairdevice.ejs');
@@ -21,11 +54,47 @@ app.get('/pairdevice',(req,res)=>{
 
 app.post('/pairdevice',(req,res)=>{
   
-    const {ipaddress,password} = req.body;
+    const {ipaddress,pairingport,password,port} = req.body;
     //res.send(`Entered ip: ${ipaddress} Entered key: ${password}`);
-    exec(`adb pair ${ipaddress} ${password}`)
-    .then(res.redirect('/'))
-    .catch(err=>{console.log(err)})
+    exec(`adb pair ${ipaddress}:${pairingport} ${password}`,(error, stdout,stderr)=>{
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).send('Error occurred during pairing');
+      }
+      if (stderr) {
+        console.error(`ADB Error: ${stderr}`);
+        return res.status(500).send('ADB Error occurred during pairing');
+      }
+      console.log(`Pairing successful: ${stdout}`);
+      console.log(ipaddress)
+      res.send(`Pairing successful <a href='/connectDevice'>connect Device</a>`);
+    })
+    
+
+app.get('/connectDevice',(req,res)=>{
+    res.render('connectDevice');
+})    
+app.post('/connectDevice',(req,res)=>{
+   
+  exec(`adb connect ${ipaddress}:${port}`,(error, stdout,stderr)=>{
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).send('Error occurred during pairing');
+      }
+      if (stderr) {
+        console.error(`ADB Error: ${stderr}`);
+        return res.status(500).send('ADB Error occurred during pairing');
+      }
+      console.log(`Connection successful: ${stdout}`);
+      res.send(`Pairing & Connection successful <a href='/'>home</a>`);
+    })
+})
+
+
+
+    // .then(res.redirect('/'))
+    // .then(res.send('paired successfully!'))
+    // .catch(err=>{console.log(err)})
 });
 
 
